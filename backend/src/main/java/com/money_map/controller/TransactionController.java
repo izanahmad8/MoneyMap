ea2b3352b21a1b67cf13c4f2977b27a0f6ddc35d
Izan Ahmad <izan.khan596@gmail.com>
2025-09-28 02:04:52 +0530

    changes

--- backend/src/main/java/com/money_map/controller/TransactionController.java
+++ backend/src/main/java/com/money_map/controller/TransactionController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.money_map.utils.TransactionResponse;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/transaction")
 public class TransactionController {
     @Autowired
